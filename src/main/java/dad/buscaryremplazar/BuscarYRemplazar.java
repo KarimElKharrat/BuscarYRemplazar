1857994db26302a24222fc62a658a7025a706218
KarimElKharrat <113608883+KarimElKharrat@users.noreply.github.com>
2022-10-12 19:49:54 +0100

    Refinado de código y uso de hbox a parte del gridbox

--- src/main/java/dad/buscaryremplazar/BuscarYRemplazar.java
+++ src/main/java/dad/buscaryremplazar/BuscarYRemplazar.java
@@ -11,20 +11,18 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BuscarYRemplazar extends Application {
 
 	//view
-	
-	private GridPane root;
+	private HBox root;
 	private TextField buscarText, reemplazarText;
 	private CheckBox mayusYMinusCheck, haciaAtrasCheck, regularExpCheck, resaltarCheck;
-	private Button buscarButton, reemplazarButton, reemplazarTodoButton, cerrarButton, ayudaButton;
 	private VBox buttonsPanel;
-	
-	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -34,16 +32,13 @@ public class BuscarYRemplazar extends Application {
 		reemplazarText = new TextField();
 		
 		//botones
-		buscarButton = new Button("Buscar");
-		reemplazarButton = new Button("Reemplazar");
-		reemplazarTodoButton = new Button("Reemplazar todo");
-		cerrarButton = new Button("Cerrar");
-		ayudaButton = new Button("Ayuda");
-
-		buscarButton.setMaxWidth(Double.MAX_VALUE);
-		reemplazarButton.setMaxWidth(Double.MAX_VALUE);
-		cerrarButton.setMaxWidth(Double.MAX_VALUE);
-		ayudaButton.setMaxWidth(Double.MAX_VALUE);
+		buttonsPanel = new VBox();
+		String[] botonesTexto = {"Buscar", "Reemplazar", "Reemplazar todo", "Cerrar", "Ayuda"};
+		for(String s:botonesTexto) {
+			Button b = new Button(s);
+			b.setMaxWidth(Double.MAX_VALUE);//todos los botones con el maxwidth = width del más largo
+			buttonsPanel.getChildren().add(b);
+		}
 		
 		//check boxes
 		mayusYMinusCheck = new CheckBox("Mayúsculas y Minúsculas");
@@ -52,40 +47,48 @@ public class BuscarYRemplazar extends Application {
 		resaltarCheck = new CheckBox("Resaltar resultados");
 		
 		//vbox con los botones
-		buttonsPanel = new VBox(buscarButton, reemplazarButton, reemplazarTodoButton, cerrarButton, ayudaButton);
-		buttonsPanel.setAlignment(Pos.CENTER);
-		buttonsPanel.setPadding(new Insets(0,5,0,5));
+//		buttonsPanel.setStyle("-fx-background-color: red");
+		buttonsPanel.setAlignment(Pos.TOP_CENTER);
+		buttonsPanel.setPadding(new Insets(5,5,5,5));
 		buttonsPanel.setSpacing(5);
+		buttonsPanel.setMinWidth(Control.USE_PREF_SIZE);
 		
 		//grid pane con los check boxes
 		GridPane checkPanel = new GridPane();
+//		checkPanel.setStyle("-fx-background-color: yellow");
 		checkPanel.setHgap(5);
 		checkPanel.setVgap(5);
+		checkPanel.setMaxWidth(Control.USE_PREF_SIZE);
 		checkPanel.addRow(0, mayusYMinusCheck, haciaAtrasCheck);
 		checkPanel.addRow(1, regularExpCheck, resaltarCheck);
 		
+		// gridpane con los elementos del formulario
+		GridPane formularioPanel = new GridPane();
+//		gridpane.setStyle("-fx-background-color: aqua");
+		formularioPanel.setPadding(new Insets(5,5,5,5));
+		formularioPanel.setHgap(5);
+		formularioPanel.setVgap(5);
+		formularioPanel.addRow(0, new Label("Buscar:"), buscarText, buttonsPanel);
+		formularioPanel.addRow(1, new Label("Reemplazar con:"), reemplazarText);
+		formularioPanel.addRow(2, new Label(""), checkPanel);
+
 		//root con todo
-		root = new GridPane();
-		root.setPadding(new Insets(5,5,5,5));
-		root.setHgap(5);
-		root.setVgap(5);
-		root.addRow(0, new Label("Buscar:"), buscarText, buttonsPanel);
-		root.addRow(1, new Label("Reemplazar con:"), reemplazarText);
-		root.addRow(2, new Label(""), checkPanel);
+		root = new HBox(formularioPanel, buttonsPanel);
+		HBox.setHgrow(formularioPanel, Priority.ALWAYS);
 		
 		ColumnConstraints [] cols = {
 			new ColumnConstraints(), 
 			new ColumnConstraints(), 
 			new ColumnConstraints()	
 		};
-		
-		root.getColumnConstraints().setAll(cols);
+		formularioPanel.getColumnConstraints().setAll(cols);
+
+		cols[1].setHgrow(Priority.ALWAYS);
 		cols[2].setMinWidth(Control.USE_PREF_SIZE);
 		
-		GridPane.setRowSpan(buttonsPanel, 3);
 		
 		primaryStage.setTitle("BuscarYRemplazar");
-		primaryStage.setScene(new Scene(root, 500, 300));
+		primaryStage.setScene(new Scene(root, 550, 250));
 		primaryStage.show();
 	}
 
